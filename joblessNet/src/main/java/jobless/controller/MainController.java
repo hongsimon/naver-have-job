845a95fe80c5db5195960dd11c092008caa12c41
KOITT02A <KOITT02A@KOITT02A-PC>
2018-11-29 21:23:30 +0900

    user컨트롤러 추가

--- joblessNet/src/main/java/jobless/controller/MainController.java
+++ joblessNet/src/main/java/jobless/controller/MainController.java
@@ -10,15 +10,4 @@ public class MainController {
 		return "test";
 	}
 	
-	@RequestMapping(value="/main")
-	public String controllerMain() {
-		System.out.println("메인 페이지");
-		return "main";
-	}
-	
-	@RequestMapping(value="/join")
-	public String controllerJoin() {
-		System.out.println("회원가입 페이지");
-		return "join";
-	}
 }
