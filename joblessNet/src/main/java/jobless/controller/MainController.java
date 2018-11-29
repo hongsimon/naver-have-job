fe2d05591c3cc08957e510b9913242d1f0085da0
KOITT02A <KOITT02A@KOITT02A-PC>
2018-11-29 21:06:48 +0900

    merge

--- joblessNet/src/main/java/jobless/controller/MainController.java
+++ joblessNet/src/main/java/jobless/controller/MainController.java
@@ -9,4 +9,16 @@ public class MainController {
 	public String controllerTest() {
 		return "test";
 	}
+	
+	@RequestMapping(value="/main")
+	public String controllerMain() {
+		System.out.println("메인 페이지");
+		return "main";
+	}
+	
+	@RequestMapping(value="/join")
+	public String controllerJoin() {
+		System.out.println("회원가입 페이지");
+		return "join";
+	}
 }
