af4b2d80ce17a1295485aa1aeff02feb1ea58325
01057630422 <01057630422@DESKTOP-42A6J56>
2018-12-06 21:00:03 +0900

    readBoardApply 수정

---
+++ joblessNet/src/main/java/jobless/exception/ReadBoardApplyException.java
@@ -0,0 +1,8 @@
+package jobless.exception;
+
+public class ReadBoardApplyException extends RuntimeException {
+
+	public ReadBoardApplyException(String msg) {
+		super(msg);
+	}
+}
--- joblessNet/src/main/java/jobless/service/board/ReadBoardApplyServiceImpl.java
+++ joblessNet/src/main/java/jobless/service/board/ReadBoardApplyServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import jobless.dao.BoardApplyDAO;
+import jobless.exception.ReadBoardApplyException;
 import jobless.model.BoardApplyVO;
 
 public class ReadBoardApplyServiceImpl implements ReadBoardApplyService {
@@ -15,18 +16,27 @@ public class ReadBoardApplyServiceImpl implements ReadBoardApplyService {
 	@Override
 	public BoardApplyVO readBoardApplyByBoardApplyId(int boardApplyId) {
 		BoardApplyVO vo = boardApplydao.read(boardApplyId);
+		if(vo == null) {
+			throw new ReadBoardApplyException("boardApplyId에 해당하는 컬럼을 가져오는데에 실해하였습니다.");
+		}
 		return vo;
 	}
 
 	@Override
 	public List<BoardApplyVO> readBoardApplyList() {
 		List<BoardApplyVO> list = boardApplydao.readAll();
+		if(list == null) {
+			throw new ReadBoardApplyException("boardApply목록을 가져오는데에 실해하였습니다.");
+		}
 		return list;
 	}
 
 	@Override
 	public List<BoardApplyVO> readBoardApplyOwnList(int ownerId) {
 		List<BoardApplyVO> list = boardApplydao.readChoosedAll(ownerId);
+		if(list == null) {
+			throw new ReadBoardApplyException("userId로 boardApply에 해당하는 컬럼을 가져오는데에 실해하였습니다.");
+		}
 		return list;
 	}
 
