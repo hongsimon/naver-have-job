296baa49f679c71f37a3d5b3da8eb99983649984
유대헌 <dev0315@naver.com>
2018-12-06 18:38:07 +0900

    clip exception 작업 진행중

---
+++ joblessNet/src/main/java/jobless/exception/ClipNotFoundException.java
@@ -0,0 +1,8 @@
+package jobless.exception;
+
+public class ClipNotFoundException extends RuntimeException{
+	
+	public ClipNotFoundException(String msg) {
+		super(msg);
+	}
+}
--- joblessNet/src/main/java/jobless/service/clip/ReadClipServiceImpl.java
+++ joblessNet/src/main/java/jobless/service/clip/ReadClipServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import jobless.dao.ClipDAO;
+import jobless.exception.ClipNotFoundException;
 import jobless.model.ClipVO;
 
 @Repository("readClipService")
@@ -16,12 +17,19 @@ public class ReadClipServiceImpl implements ReadClipService{
 	
 	@Override
 	public List<ClipVO> readAllClip() {
-		return clipdao.readAll();
+		List<ClipVO> clip = clipdao.readAll();
+		if(clip == null) {
+			throw new ClipNotFoundException("readAll 할 Clip이 없습니다.");
+		}
+		return clip;
 	}
 
 	@Override
 	public List<ClipVO> readClipByUserId(int userId) {
 		List<ClipVO> clipList = clipdao.readWriterAll(userId);
+		if(clipList == null) {
+			throw new ClipNotFoundException("readAll 할 Clip이 없습니다.");
+		}
 		return clipList;
 	}
 
@@ -43,6 +51,9 @@ public class ReadClipServiceImpl implements ReadClipService{
 	public ClipVO readClip(int boardId) {
 		// TODO Auto-generated method stub
 		ClipVO clip = clipdao.read(boardId);
+		if(clip == null) {
+			throw new ClipNotFoundException("readAll 할 Clip이 없습니다.");
+		}
 		return clip;
 	}
 
